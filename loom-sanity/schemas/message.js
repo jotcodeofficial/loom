b03e05cc41bc52364534ea3c224a830094f08209
Administrator <admin@jotcode.com>
2022-07-16 18:53:50 +0200

    add sanity studio

---
+++ loom-sanity/schemas/message.js
@@ -0,0 +1,23 @@
+export default {
+  name: "message",
+  type: "document",
+  title: "Message",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+    },
+    {
+      name: "email",
+      type: "string",
+    },
+    {
+      name: "subject",
+      type: "string",
+    },
+    {
+      name: "message",
+      type: "text",
+    },
+  ],
+};
---
+++ loom-sanity/schemas/post.js
@@ -0,0 +1,78 @@
+export default {
+  name: "post",
+  title: "Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: "keep the titles short!",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: { type: "author" },
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+    {
+      name: "body",
+      title: "Body",
+      type: "blockContent",
+    },
+    {
+      name: "showThumbnailInPost",
+      title: "Show Thumbnail In Post",
+      description: "This will show the thumbnail image at the top of the post",
+      type: "boolean",
+      initialValue: true,
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`,
+      });
+    },
+  },
+};
